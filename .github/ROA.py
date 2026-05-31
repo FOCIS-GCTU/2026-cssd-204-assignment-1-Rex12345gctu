ba122301e62df09e376f811ad579ec29933d29ab
Rex12345gctu <rexasare502@gmail.com>
2026-05-31 15:39:56 -0400

    Add files via upload

---
+++ .github/ROA.py
@@ -0,0 +1,35 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Rexford Owusu Asare
+# STUDENT ID: 2425403179
+# Email: rexasare502@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, REXFORD OWUSU ASARE, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print stylized initials
+
+    print()
+    print("...ROA")
+    print()
+
+    print("...RRRRRRRRRR...........OOOOOOOO...........AAAAAAAAAA......")
+    print("...RRRRRRRRRR..........OOOOOOOOOO..........AAAAAAAAAA......")
+    print("...RR......RR.........OOO......OOO.........AA......AA......")
+    print("...RR......RR.........OO........OO.........AA......AA......")
+    print("...RRRRRRRRRR.........OO........OO.........AAAAAAAAAA......")
+    print("...RRRRRRRRR..........OO........OO.........AAAAAAAAAA......")
+    print("...RR.....RR..........OO........OO........AA........AA.....")
+    print("...RR......RR.........OOO......OOO........AA........AA.....")
+    print("...RR.......RR...**....OOOOOOOOOO... **...AA........AA...**")
+    print("...RR.......RR...**.....OOOOOOOO.....**...AA........AA...**")
+
+    print()
+
+
+main()
